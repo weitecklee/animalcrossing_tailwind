20ccd7b1c17bb9eac0b340934524dc1210e081dd
arkteck <arkteck@gmail.com>
2024-10-03 23:42:05 -0400

    set up index page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Coustard, Montserrat } from "next/font/google";
-import "./globals.css";
-import TopBar from "./topBar";
+import type { Metadata } from 'next';
+import { Coustard, Montserrat } from 'next/font/google';
+import './globals.css';
+import TopBar from './topBar';
 
 const montserrat = Montserrat({
-  weight: "variable",
-  subsets: ["latin"],
-  variable: "--font-montserrat",
+  weight: 'variable',
+  subsets: ['latin'],
+  variable: '--font-montserrat',
 });
 const coustard = Coustard({
-  weight: ["400"],
-  subsets: ["latin"],
-  variable: "--font-coustard",
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-coustard',
 });
 
 export const metadata: Metadata = {
-  title: "My Animal Crossing Island",
+  title: 'My Animal Crossing Island',
   description:
-    "Showcase of my Animal Crossing: New Horizons island and its villagers",
+    'Showcase of my Animal Crossing: New Horizons island and its villagers',
 };
 
 export default function RootLayout({
@@ -30,10 +30,10 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} ${coustard.variable} antialiased`}
       >
-        <div className="container mx-auto max-w-4xl px-4">
+        <div className="container mx-auto max-w-7xl px-4 mt-20">
           <TopBar />
+          {children}
         </div>
-        {children}
       </body>
     </html>
   );
