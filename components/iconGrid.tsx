63bbc0b8753d9f4446f5886c5dd4a6552f1b947a
arkteck <arkteck@gmail.com>
2024-10-14 09:42:52 -0400

    setting up stats page

--- components/iconGrid.tsx
+++ components/iconGrid.tsx
@@ -8,10 +8,20 @@ export default function IconGrid({
   traitData?: Trait;
   villagers?: string[];
 }) {
+  if (!!traitData) {
+    return (
+      <div className="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(64px,1fr))] gap-1 py-2">
+        {traitData.villagers.map((villager) => (
+          <VillagerIcon key={villager} villager={villager} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(64px,1fr))] gap-1 py-2">
       {villagers!.map((villager) => (
-        <VillagerIcon villager={villager} />
+        <VillagerIcon key={villager} villager={villager} />
       ))}
     </div>
   );
