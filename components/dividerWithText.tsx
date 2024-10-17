b26cb1b797d744e43554191bfaeaa511947a2610
Wei-Teck Lee <arkteck@gmail.com>
2024-10-17 14:38:16 -0400

    Dev (#10)
    * edit topBar links
    
    * tooltip with flowbite

--- components/dividerWithText.tsx
+++ components/dividerWithText.tsx
@@ -1,17 +1,28 @@
+import { ReactNode } from 'react';
+
 export default function DividerWithText({
   text,
   className,
+  content,
 }: {
   text: string;
   className?: string;
+  content?: ReactNode;
 }) {
   return (
     <div className={`flex items-center ${className}`}>
       <div className="flex-grow border-t border-gray-300"></div>
-      <span className="mx-2 text-sm font-coustard bg-alternate py-2 px-3 rounded-full">
+      <span className="mx-2 text-sm font-coustard bg-alternate py-2 px-3 rounded-full flex items-center gap-2">
         {text}
+        {content}
       </span>
       <div className="flex-grow border-t border-gray-300"></div>
     </div>
   );
+}
+
+export interface DividerWithTextProps {
+  text: string;
+  className?: string;
+  content?: ReactNode;
 }
