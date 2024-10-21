ed753f01034e446d8e144ea4c93f3e98fa3a0ae6
arkteck <arkteck@gmail.com>
2024-10-21 12:59:31 -0400

    tooltip content and behavior

--- components/villagerTooltip.tsx
+++ components/villagerTooltip.tsx
@@ -1,12 +1,24 @@
+import IconWithText from '@/components/iconWithText';
 import { DataContext } from '@/lib/dataContext';
+import { dayOrDays, rgbDataURL } from '@/lib/functions';
 import nookipediaData from '@/lib/nookipediaData';
+import {
+  ArrowRightEndOnRectangleIcon,
+  ArrowRightStartOnRectangleIcon,
+} from '@heroicons/react/16/solid';
+import { ClockIcon } from '@heroicons/react/24/outline';
 import { Popover, PopoverProps } from 'flowbite-react';
-import { useContext } from 'react';
+import Image from 'next/image';
+import { useContext, useRef, useState } from 'react';
+import CRBadge from './crBadge';
 
 interface VillagerTooltipProps extends Omit<PopoverProps, 'content'> {
   children: React.ReactNode;
   villager: string;
 }
+
+let noDelay = false;
+let noDelayID: NodeJS.Timeout | null = null;
 
 export default function VillagerTooltip({
   villager,
@@ -14,21 +26,82 @@ export default function VillagerTooltip({
   ...props
 }: VillagerTooltipProps) {
   const { historyMap } = useContext(DataContext);
-  const villagerData = nookipediaData.get(villager);
+  const villagerData = nookipediaData.get(villager)!;
   const history = historyMap.get(villager);
+
+  const [open, setOpen] = useState(false);
+  const timeoutID = useRef<NodeJS.Timeout | null>(null);
 
   return (
     <Popover
       {...props}
       aria-labelledby="villager-tooltip"
       content={
-        <div>
-          <p>{villager}</p>
+        <div className="bg-alternate p-4">
+          <div className="relative">
+            <Image
+              src={villagerData.nh_details.icon_url}
+              alt={villager}
+              title={villager}
+              height={128}
+              width={128}
+              style={{
+                cursor: 'pointer',
+              }}
+              placeholder="blur"
+              blurDataURL={rgbDataURL(villagerData.title_color)}
+            />
+            {history?.currentResident && <CRBadge />}
+          </div>
+          <p className="font-coustard text-lg">{villager}</p>
+          {history && (
+            <>
+              <IconWithText
+                Icon={ArrowRightEndOnRectangleIcon}
+                text={history.startDateString}
+              />
+              {!history.currentResident && (
+                <IconWithText
+                  Icon={ArrowRightStartOnRectangleIcon}
+                  text={history.endDateString}
+                />
+              )}
+              <IconWithText
+                Icon={ClockIcon}
+                text={dayOrDays(history.duration)}
+              />
+            </>
+          )}
         </div>
       }
-      trigger="hover"
+      arrow={false}
+      open={open}
+      onOpenChange={setOpen}
     >
-      {children}
+      <div
+        onMouseEnter={() => {
+          if (timeoutID.current) clearTimeout(timeoutID.current);
+          if (noDelayID) clearTimeout(noDelayID);
+          if (noDelay) {
+            setOpen(true);
+          } else {
+            timeoutID.current = setTimeout(() => {
+              noDelay = true;
+              setOpen(true);
+            }, 500);
+          }
+        }}
+        onMouseLeave={() => {
+          if (timeoutID.current) clearTimeout(timeoutID.current);
+          if (noDelayID) clearTimeout(noDelayID);
+          setOpen(false);
+          noDelayID = setTimeout(() => {
+            noDelay = false;
+          }, 500);
+        }}
+      >
+        {children}
+      </div>
     </Popover>
   );
 }
